eab6a6a973996c4ad537152e0c49233ed8214380
Zaman Ali <138426327+zamanali423@users.noreply.github.com>
2025-07-28 15:37:35 +0500

    Update Register.js

--- frontend/src/admin/pages/Register.js
+++ frontend/src/admin/pages/Register.js
@@ -1,6 +1,51 @@
-import React from "react";
+import React, { useContext, useState } from "react";
+import { useNavigate } from "react-router";
+import { productContext } from "../../context/productContext/productContext";
+import toast from "react-hot-toast";
 import Login from "./Login";
 const Register = () => {
+  const navigate = useNavigate();
+  const { setToken, url } = useContext(productContext);
+  const [inputFields, setInputFields] = useState({
+    email: "",
+    password: "",
+  });
+  const [isSubmitting, setIsSubmitting] = useState(false);
+
+  const handleInput = (e) => {
+    setInputFields({ ...inputFields, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setIsSubmitting(true);
+    try {
+      const response = await fetch(`${url}/users/register`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(inputFields),
+      });
+      const data = await response.json();
+      if (!response.ok) {
+        toast.error(data.msg);
+        setIsSubmitting(false);
+        return;
+      }
+
+      const token = data.token;
+      setToken(token);
+      localStorage.setItem("token", token);
+      navigate("/dashboard");
+      toast.success(data.msg);
+    } catch (error) {
+      console.error("Login error:", error);
+      toast.error("An error occurred. Please try again.");
+    } finally {
+      setIsSubmitting(false);
+    }
+  };
   return (
     <>
       <div className="block-space block-space--layout--after-header"></div>
@@ -14,7 +59,7 @@ const Register = () => {
               <div className="card flex-grow-1 mb-0 ml-0 ml-lg-3 mr-0 mr-lg-4">
                 <div className="card-body card-body--padding--2">
                   <h3 className="card-title">Register</h3>
-                  <form>
+                  <form onSubmit={handleSubmit}>
                     <div className="form-group">
                       <label for="signup-email">Email address</label>
                       <input
@@ -22,6 +67,10 @@ const Register = () => {
                         type="email"
                         className="form-control"
                         placeholder="customer@example.com"
+                          name="email"
+                  value={inputFields.email}
+                  onChange={handleInput}
+                  required
                       />
                     </div>
                     <div className="form-group">
@@ -31,6 +80,10 @@ const Register = () => {
                         type="password"
                         className="form-control"
                         placeholder="Secret word"
+                          name="password"
+                  value={inputFields.password}
+                  onChange={handleInput}
+                  required
                       />
                     </div>
                     <div className="form-group">
